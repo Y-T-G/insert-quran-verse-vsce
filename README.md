fcd686facee785a3e4feea00971a701a5da98a7a
Y-T-G <yaseensinbox@gmail.com>
2023-08-09 22:26:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# insert-quran-verse README
+
+Insert Quran Verse allows you to easily use partial search to find the verse(s) you are looking for and allows you to insert them into your document. It is based on the InDesign [quran-plugin](https://www.sadeea.de/quran-plugin) by Mohammad Sadee.
+
+## Usage
+
+![How To](images/insert-quran-verse-demo.gif)
+
+1. To insert a verse, open the Command Pallete and search for "Insert Quran verse" and press enter.
+2. Then write the word or portion of a verse to be searched and press enter.
+3. Select the verse you want to insert and press enter again to insert.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `insert-quran-verse.prefix`: Add a prefix to inserted verses.
+* `insert-quran-verse.suffix`: Add a suffix to inserted verses.
+
+## Known Issues
+
+The extension might fail to look up a verse due to how diacritics work in
+Arabic language. It is recommended to search with a short word or a portion of
+a verse.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+Initial release of Insert Quran Verse.
---
+++ src/extension.ts
@@ -0,0 +1,75 @@
+import * as vscode from 'vscode';
+
+interface Verse {
+    id: number;
+    jozz: number;
+    sora: number;
+    sora_name_en: string;
+    page: number;
+    line_start: number;
+    line_end: number;
+    aya_no: number;
+    aya_text: string;
+    aya_text_emlaey: string;
+}
+
+const quranVerses = <Verse[]> require('../hafsData_v18.js');
+
+function removeDiacritics(text: string): string {
+	//remove special characters
+	text = text.replace(/([^\u0621-\u063A\u0641-\u064A\u0660-\u0669a-zA-Z 0-9])/g, '');
+
+	return text;
+  }
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand('insert-quran-verse.searchQuran', async () => {
+    const searchText = await vscode.window.showInputBox({ prompt: 'Enter Arabic word or phrase to search' });
+    if (searchText) {
+      let results = quranVerses.filter((verse: { id: number; aya_text_emlaey: string; aya_text: string }) =>
+	  verse.aya_text_emlaey.includes(removeDiacritics(searchText))
+      );
+	
+	  // fallback attempt with diacritics
+	  if (results.length == 0) {
+		results = quranVerses.filter((verse: { id: number; aya_text: string }) =>
+		verse.aya_text.includes(searchText)
+		);
+	}
+
+      if (results.length > 0) {
+        const verseOptions = results.map((verse: {sora: number; aya_no: number; aya_text: string }) => {
+          return {
+            label: `${verse.sora}:${verse.aya_no}`,
+            description: verse.aya_text,
+          };
+        });
+
+        const selectedVerse = await vscode.window.showQuickPick(verseOptions, {
+          placeHolder: 'Select a verse to insert',
+        });
+
+        if (selectedVerse) {
+          const activeEditor = vscode.window.activeTextEditor;
+          if (activeEditor) {
+            const config = vscode.workspace.getConfiguration('insert-quran-verse');
+            const prefix = config.get('prefix') || '';
+            const suffix = config.get('suffix') || '';
+
+            const textToInsert = `${prefix}${selectedVerse.description}${suffix}`;
+
+            activeEditor.edit((editBuilder: vscode.TextEditorEdit) => {
+              editBuilder.insert(activeEditor.selection.active, textToInsert);
+            });
+          }
+		}
+      } else {
+        vscode.window.showInformationMessage('No matching verses found.');
+      }
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
